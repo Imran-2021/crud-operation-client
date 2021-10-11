eb4f8d9a18b28d96b8fa73cf68c9df653d6fc005
Imran-2021 <mdimranulhaque.8dec20@gmail.com>
2021-10-11 21:03:42 +0600

    setup done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Admin from "./Components/Admin/Admin";
+import Home from "./Components/Home/Home";
+import NavBar from "./Components/NavBar/NavBar";
+import NoMatch from "./Components/Nomatch/NoMatch";
+import Table from "./Components/Table/Table";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar/>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route  path="/home">
+          <Home />
+        </Route>
+        <Route path="/users">
+          <Table />
+        </Route>
+        <Route path="/admin">
+          <Admin/>
+        </Route>
+        <Route exact path="*">
+          <NoMatch />
+        </Route>
+      </Switch>
+  </Router>
   );
 }
 
